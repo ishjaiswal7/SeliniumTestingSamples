0ff898d4347ff8505c08356ade2d0bec0d948c7b
Ish Jaiswal <ishjaiswal7@gmail.com>
2025-02-12 19:33:11 +0530

    initial commit

---
+++ SeliniumTestingSamples/src/main/java/selinium/SeliniumTestingSamples/App.java
@@ -0,0 +1,13 @@
+package selinium.SeliniumTestingSamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
