18d6afc4f7fd3ec77ee34be52d0274523c37a8d3
Ish Jaiswal <ishjaiswal7@gmail.com>
2025-02-16 10:44:58 +0530

    example2_1 added

---
+++ SeliniumTestingSamples/src/test/java/selinium/SeliniumTestingSamples/Examples/Example2_1.java
@@ -0,0 +1,24 @@
+package selinium.SeliniumTestingSamples.Examples;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+//Firefox Browser
+public class Example2_1 {
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		//Go to google home page
+		driver.get("https://www.google.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6000));
+		System.out.println("Website launched successfully");
+		String getTitle = driver.getTitle();
+		System.out.println("Title of home page: " + getTitle);
+		driver.close();
+		
+	}
+}
+
