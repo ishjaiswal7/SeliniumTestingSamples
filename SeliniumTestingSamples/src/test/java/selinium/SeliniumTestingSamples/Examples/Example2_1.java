5c9fa3327ccb66a31709f553a0004497f1cfbf95
Ish Jaiswal <ishjaiswal7@gmail.com>
2025-03-01 14:27:58 +0530

    updated 2_1

--- SeliniumTestingSamples/src/test/java/selinium/SeliniumTestingSamples/Examples/Example2_1.java
+++ SeliniumTestingSamples/src/test/java/selinium/SeliniumTestingSamples/Examples/Example2_1.java
@@ -11,7 +11,7 @@ public class Example2_1 {
 		
 		WebDriver driver = new FirefoxDriver();
 		
-		//Go to google home page
+		// Go to google home page
 		driver.get("https://www.google.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6000));
 		System.out.println("Website launched successfully");
